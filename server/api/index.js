de9c0eaddd1a876d03e84f40c6cb60f6ffc0c43e
ArrMannDev <arrmann632025@gmail.com>
2026-08-14 16:57:37 +0630

    fix:route error

--- server/api/index.js
+++ server/api/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({
     message: "DPS Map Download API is running",
   });
